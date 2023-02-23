0edbc4086ed98ec66f8300106c30e8967ff9525e
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-23 15:37:36 +0530

    removed unwanted files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import * as React from 'react'
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   const name='Sandeep Kumar';
   const isLoggedin=false;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {!isLoggedin && <p>Hello World</p>}
-          {isLoggedin && <p>Hello {name}</p>}
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Cart</h1>
     </div>
   );
 }
