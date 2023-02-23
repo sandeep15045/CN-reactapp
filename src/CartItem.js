c7d2395447097244c5ddf2053f0fdad19ea18d67
Sandeep Kumar <sandeep15045@gmail.com>
2023-02-23 16:09:04 +0530

    buttons, state, parent constructor

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,6 +1,16 @@
 import React from "react";
 
+
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            price:999,
+            title:'Phone',
+            qty:1,
+            img:''
+        }
+    }
     render(){
         return(
             <div className="cart-item">
@@ -8,11 +18,14 @@ class CartItem extends React.Component{
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fonctSize:25}}>Phone</div>
+                    <div style={{fonctSize:25}}>{this.state.title}</div>
                     <div style={{color:'#777'}}>Rs 999</div>
                     <div style={{color:'#777'}}>Qty: 1</div>
                     <div className="cart-items-actions">
                         {/*Buttons*/} 
+                        <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png"/>
+                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/9758/9758171.png"/>
+                        <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/5974/5974771.png"/>
                     </div>
                 </div>
             </div>
